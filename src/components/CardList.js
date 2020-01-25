642dbebf40d5e991f1b344bc9179db11cd239447
asfand687 <asfandyar687@gmail.com>
2020-01-25 12:49:59 +0500

    removed the error line which was always true

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,6 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  if (true) {
-    throw new Error('NOOOOOO!');
-  }
   const cardsArray = robots.map((user, i) => {
     return (
       <Card key={user.id} id={user.id} name={user.name} email={user.email} />
